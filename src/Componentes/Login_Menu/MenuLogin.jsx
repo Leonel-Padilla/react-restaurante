aede56709eea7f53b5f48faedb5daf481556042b
Leonel-Padilla <axcel.sanchez1@ujcv.edu.hn>
2022-03-16 15:20:25 -0600

    Login

--- src/Componentes/Login_Menu/MenuLogin.jsx
+++ src/Componentes/Login_Menu/MenuLogin.jsx
@@ -2,77 +2,48 @@ import React, {useState} from 'react'
 import { Link, Navigate, useNavigate, useNavigationType } from 'react-router-dom'
 import { Row, Button, Modal, Text, Input } from '@nextui-org/react';
 import FondoLogin from '../../img/FondoLogin.jpg'
+import axios from 'axios';
 
+
+
+const endPointGetEmpleadoByUN = 'http://127.0.0.1:8000/api/Empleado'
 
 function MenuLogin () {
     //const [visible, setVisible] = useState(true);
-    const [nombre, setNombre] = useState('')
-    const [password, setPassword] = useState('')
+    const [nombreUsuario, setNombre] = useState('')
+    const [contrasenia, setcontrasenia] = useState('')
+    const [usuario, setUsuario] = useState({})
     const navigate = useNavigate()
 
 
 
-    const validar = ()=>{
-        console.log(nombre, password)
+    const validar = async ()=>{
+        const response = await axios.get(`${endPointGetEmpleadoByUN}U/${nombreUsuario}`)
 
-        if (nombre == 'admin' && password == 'admin'){
-            navigate('/MenuPrincipal')
-            
-            sessionStorage.setItem('userName', nombre)
-            sessionStorage.setItem('contrasenia', password)
-        }else{
-            alert('Credenciales Invalidas')
-        }
+        setUsuario(response.data)
+
+        console.log(nombreUsuario)
+        console.log(usuario)
     }
 
     return (
-        
-            /*<div className="container">
-                <div className='container border-style col-sm-6 col-md-4 col-lg-3'>
-                    <form className='form'>
-                        <h2 className="mt-5 mb-4 ms-5 ">Bienvenidos</h2>
-                        <div className="mb-3">
-                            <Input
-                            className='text-white mt-3 ms-4 form-control'
-                            underlined
-                            placeholder='Usuario'
-                            onChange={(e)=>setNombre(e.target.value)}
-                            aria-label='aria-labelledby'
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <Input
-                            className='text-white mt-3 ms-4 form-control' 
-                            underlined
-                            placeholder='contraseña'
-                            type='password'
-                            onChange={(e)=>setPassword(e.target.value)}
-                            aria-label='aria-labelledby'/>
-                        </div>
-                        <Button 
-                        className='bg-dark mt-4 ms-5'
-                        onClick={()=>validar()} 
-                        >Ingresar</Button>
-                    </form>
-                </div>
-            </div>*/
             
         <div className='body'>
             <div className='login-box'>
                 <h1>Bienvenido</h1>
-                <label className='ms-2'>Username</label>
+                <label className='ms-2'>Usuiario</label>
                 <Input 
                     className='ms-4'
                     placeholder='Usuario'
                     onChange={(e)=>setNombre(e.target.value)}
                     aria-label='aria-labelledby'
                     />
-                <label  className='ms-2'>Password</label>
+                <label className='ms-2'>Contraseña</label>
                 <Input 
                     className='ms-4'
                     placeholder='Contraseña'
-                    type="password"
-                    onChange={(e)=>setPassword(e.target.value)}
+                    type="contrasenia"
+                    onChange={(e)=>setcontrasenia(e.target.value)}
                     aria-label='aria-labelledby'
                     />
                 <Button 
